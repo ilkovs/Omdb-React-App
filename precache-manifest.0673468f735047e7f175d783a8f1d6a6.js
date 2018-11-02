647f26168ec9c3da084139d19391e73467f473d8
Sam Ilkov <samuil.ilkov90@gmail.com>
2018-11-02 11:04:08 -0400

    Updates

---
+++ precache-manifest.0673468f735047e7f175d783a8f1d6a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2e5530867fddb025101",
+    "url": "/omdb-react-app/static/js/runtime~main.d2e55308.js"
+  },
+  {
+    "revision": "17e4f201e84d09c677ab",
+    "url": "/omdb-react-app/static/js/main.17e4f201.chunk.js"
+  },
+  {
+    "revision": "920db098f8527ec2ae1d",
+    "url": "/omdb-react-app/static/js/1.920db098.chunk.js"
+  },
+  {
+    "revision": "17e4f201e84d09c677ab",
+    "url": "/omdb-react-app/static/css/main.3652d372.chunk.css"
+  },
+  {
+    "revision": "962735325fb10a68a9bb9fcf69424252",
+    "url": "/omdb-react-app/index.html"
+  }
+];
